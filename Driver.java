b069df1f4df3580b8b02c3b4d58cb2a5b7cf3450
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-09-25 13:51:20 -0400

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,53 @@
+import java.util.*; 
+import java.io.*;
+/**
+ * This is the driver of Sort class. By run the main method, this
+ * program will generates a random number which decides which sorting
+ * algorithms it will use. The results are the same. 
+ *
+ * @author (Zhiyan Yu)
+ * @version (9/25/2020)
+ */
+public class Driver
+{
+    public static void main(String[] args) throws IOException{
+        Sort sort = new Sort(); 
+        int r = (int) (Math.random() * 3);
+        
+        int[] numbers; 
+        String s = " ", p = " "; 
+        
+        //allow user to input the length of the array
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the length of the array :"); 
+        int num = sc.nextInt(); 
+        numbers = new int[num]; 
+        
+        //allow user to input the array
+        for(int i = 0; i < num; ++i){
+            System.out.print("Input the array: ");
+            numbers[i] = sc.nextInt(); 
+        }
+        
+        //print the unsorted array
+        for (int i = 0; i < num; ++i){
+            System.out.print("Unsorted " + numbers[i] + " \n"); 
+        }
+        
+        if(r == 0){
+            sort.insertionSort(numbers); 
+            p = "Insertion Sort: ";
+        }else if(r == 1){
+            sort.bubbleSort(numbers);
+            p = "Bubble Sort: ";
+        }else if(r == 2){
+            sort.mergeSort(numbers, 0, numbers.length-1);
+            p = "Merge Sort: "; 
+        }
+        
+        //Print the sorted array
+        for (int i = 0; i < num; ++i){
+            System.out.print(p + numbers[i] + " \n"); 
+        }
+    }
+}
--- Sort.java
+++ Sort.java
@@ -1,16 +1,19 @@
 
 /**
- * Write a description of class Sort here.
+ * This class contains code for different sorting algorithms which
+ * including insertion sort, bubble sort and merge sort. 
  *
  * @author (Zhiyan Yu)
- * @version (9/24/2020)
+ * @version (9/24/2020-9/25/2020)
  */
-public class Sort
+public class Sort 
 {
-    int numbers[]; 
-    int len = numbers.length; 
-    private void insertionSort(int arr[], int n){
-        n = arr.length;
+    /**
+     * This is code for insertion sort algorithm. 
+     * @param - interger array, uses for sorting.
+     */
+    public void insertionSort(int arr[]){
+        int n = arr.length;
         for(int i = 1; i < n; ++i){
             int key = arr[i];
             int j = i -1; 
@@ -22,19 +25,94 @@ public class Sort
         }
     }
     
-    public String runAndResult(int r){
-        Sort sort = new Sort();
-        int i; 
-        int l = len; 
-        String s = " "; 
-        if(r == 0){
-            sort.insertionSort(numbers,len);
-
-            for(i = 0; i < l; ++i){
-               s = numbers[i] + " ";
+    /**
+     * This is code for bubble sort algorithm. 
+     * @param - interger array, uses for sorting. 
+     */
+    public void bubbleSort(int arr[]){
+        int n = arr.length;
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n -i -1; j++){
+                if(arr[j+1] < arr[j]){
+                    //swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp; 
+                }
             }
-             
         }
-        return s;
+    }
+    
+    /**
+     * This is code for merge sort algorithm. 
+     * @param - interger array, uses for sorting; 
+     * @param - integer left, represents for the number at 0;
+     * @param - integer mid, represents for the number (left+right)-1; 
+     * @param - integer right, represents for the number at arr.length-1;
+     */
+    private void merge(int arr[], int left, int mid, int right){
+        //Sized of the two subarrays
+        int n1 = mid - left +1;
+        int n2 = right - mid;
+        
+        //Create temp arrays 
+        // First subarray is arr[l..m] 
+        // Second subarray is arr[m+1..r] 
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        
+        //copy data to temp arrays
+        for(int i = 0; i < n1; ++i){
+            L[i] = arr[left + i];
+        }
+        
+        for(int j = 0; j < n2; ++j){
+            R[j] = arr[mid + j +1];
+        }
+        
+        //initial index
+        int i = 0, j =0;
+        int k = left;
+        while(i < n1 && j < n2){
+            if(L[i] <= R[j]){
+                arr[k] = L[i];
+                i++;
+            }else{
+                arr[k] = R[j];
+                j++; 
+            }
+            k++;
+        }
+        
+        //copy remaining elements 
+        while(i < n1){
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        
+        while(j < n2){
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    
+    /**
+     * This is code for merge sort algorithm. 
+     * @param - interger array, uses for sorting; 
+     * @param - integer left, represents for the number at 0;
+     * @param - integer right, represents for the number at arr.length-1;
+     */
+    public void mergeSort(int arr[], int left, int right){
+        if(left < right){
+            //find the midpoint
+            int mid = (left+right)/2;
+            //sort first and second halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid+1, right);
+            //merge the two sorted halves
+            merge(arr, left, mid, right);
+        }
     }
 }
