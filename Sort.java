06dc3eba5be69e34a07fb29dfafec7e9483dfade
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-09-27 15:52:40 -0400

    Add files via upload

--- Sort.java
+++ Sort.java
@@ -169,4 +169,38 @@ public class Sort
             heapify(arr, i, 0);
         }
     }
+    
+    /**
+     * This is partition code for the quick sort algorithm.
+     * 
+     * @param - integer array, uses for sorting;
+     * @param - integer low, the lowest in the array;
+     * @param - integer high, the highest in the array;
+     * @return - i+1 is the partition. 
+     */
+    private int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = low -1;
+        for(int j = low; j < high; j++){
+            if(arr[j] < pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp; 
+            }
+        }
+        int swap = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = swap; 
+        
+        return i+1; 
+    }
+    
+    public void quickSort(int arr[], int low, int high){
+        if(low < high){
+          int pi = partition(arr, low, high);
+          quickSort(arr, low, pi-1);
+          quickSort(arr, pi+1, high);
+       }
+    }
 }
